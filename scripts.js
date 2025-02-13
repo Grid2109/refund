73c8e0327a5b12aa3c6f6be000ade2bc4790cffe
Ingrid Lira <gridferreiradoa2109@gmail.com>
2025-02-13 15:48:01 -0300

    Creating the Function to Add a New Expense

--- scripts.js
+++ scripts.js
@@ -44,5 +44,17 @@ form.onsubmit = (event) => {
         amount: amount.value,
         created_at: new Date(),
     }
+    
+    //chama a função que adiciona novo item na lista
+    expenseAdd(newExpense)
     //console.log(newExpense)
+}
+
+//Função que adiciona uma nova despesa na lista
+function expenseAdd(newExpense) {
+ try {
+ }  catch (error) {
+     alert("Não foi possível atualizar a lista de despesas.")
+     console.log(error)
+ }
 }
