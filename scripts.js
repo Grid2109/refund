df4907ccfe2c1e457e17353a1c5a61c5da2f9dca
Ingrid Lira <gridferreiradoa2109@gmail.com>
2025-02-13 14:45:51 -0300

    Accepting only numbers

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
+////Seleciona o elemento de input pelo ID "amount" do HTML
 const amount = document.getElementById("amount")
 
+//Adiciona um evento que é acionado sempre que houver entrada de dados no input
 amount.oninput = () => {
-    
+     // Obtêm o valor atual do input e remove os caracteres não numéricos
+    let value = amount.value.replace(/\D/g, "")
+
+    //Atualiza o valor do input apenas com os números
+    amount.value = value
 }
